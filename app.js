9dcf04cee6daeac83e4c56eaad053ef75675de28
Junior-Stranner <Ju-stranner@outlook.com>
2023-12-29 09:19:27 -0300

    Making a e-comerce with my teacher Ralf Lima

---
+++ app.js
@@ -0,0 +1,55 @@
+// Importar módulo express 
+const express = require('express');
+
+//importar módulo express-handlebars
+const { engine } = require('express-handlebars'); 
+
+//importar módulo postgres
+const { Pool } = require('pg');
+
+// Criar uma instância do express
+const app = express();
+
+//adicionar bootstrap
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'))
+
+//configuração do express-handlebars
+app.engine('handlebars', engine());
+app.set('view engine','handlebars');
+app.set('views', './views');
+
+// Configuração de conexão com o PostgreSQL
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'projeto',
+  password: '123',
+  port: 5432, // Porta padrão do PostgreSQL
+});
+
+// Rota de exemplo
+app.get('/', async (req, res) => {
+  try {
+    // Exemplo de consulta
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Erro na consulta ao PostgreSQL:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+});
+
+// Iniciar o servidor
+const porta = 3000;
+app.listen(porta, () => {
+  console.log(`Servidor está ouvindo na porta ${porta}`);
+});
+
+
+//Rota Principal
+app.get('/',function(req,res){
+   res.render('formulario');
+});
+
+//Servidor
+ app.listen(8080);
